38a1e2731734fbf7ac3c6cc3d248411d4d0fc325
Michael Ryan <mtryan83@gmail.com>
2023-06-12 16:23:23 -0400

    Updates to constraints.jl and notebook
    constraints: shortened names, added allow_any field to properties struct - allows partially satisfied constraints, added structure formation constraints
    notebook - mostly just updates to support new constraints but now also save and load grids using JLD2. Will prefer loading grid over generating it

--- constraints.jl
+++ constraints.jl
@@ -79,7 +79,7 @@ end
 
 # Structure formation type constraint
 # returns Lambda
-function structure_formation_constraint(E_dens,tff,nDM)
+function struc_form_constraint(E_dens,tff,nDM)
     Lambda = ((E_dens) ./ tff) .* (1 ./ nDM) .^ 2
     return Lambda
 end
@@ -138,6 +138,7 @@ EmptyFun=Union{Function,Some{Function},Nothing}
     f_lost=nothing
     direction::cons_dir=above
     x_D::EmptyFun=nothing
+    allow_any::Bool=false
 end
 
 #Properties()
@@ -194,12 +195,12 @@ end
 # Get DwGal properties from McConnachie 2012 and Herrmann 2016
 # returns M_scale, z, Sigma_T, v_c, f_lost, direction, nothing
 function getDwGalProperties()
-    M=[1.9e+08, 3.9e+06, 3.3e+06, 230000, 270000, 940000, 810000,
+    M=sort([1.9e+08, 3.9e+06, 3.3e+06, 230000, 270000, 940000, 810000,
         1.1e+07, 9.5e+06, 1.4e+07, 2.5e+07, 1.1e+07, 6.3e+06, 2.6e+06,
         5.6e+07, 1.3e+06, 910000, 1.1e+06, 1.9e+07, 4.6e+06, 1.2e+07,
         5.4e+08, 6.5e+06, 4.2e+08, 4.4e+07, 6.1e+06, 2.3e+06, 1.2e+06,
         9.3e+07, 6.1e+06, 1.6e+07, 1.1e+07, 3.6e+07, 1.5e+08, 4.2e+07,
-        1.7e+07, 3.9e+06, 3.8e+08, 2.5e+07, 4.1e+07,]
+        1.7e+07, 3.9e+06, 3.8e+08, 2.5e+07, 4.1e+07,])
     z=zeros(size(M))
     Sigma_T = 1e-3  # Approximation from Herrmann 2016
     v_c = 100 * 1000 * 100# km/s
@@ -239,8 +240,31 @@ function getDBCProperties()
     
     direction = above
     
-    props = Properties(M,z,Sigma_T,vs,f_lost,direction, xDf)
+    props = Properties(M,z,Sigma_T,vs,f_lost,direction,xDf,false)
     return props
+end
+
+# Check that Milky Way doesn't collapse
+function getMWProperties()
+    # Only need Mhalo, z, and direction for struc formation constraint
+    # from 1804.11348
+    Mhalo = 1.54e12
+    # interested in structure formation era, so z=5,10,15?
+    z = [5 10 15]
+    direction = above
+    return Properties(;Mhalo,z,direction)
+end
+
+# Based off of 2009.05209
+function getDCOProperties()
+    # 10^6 - 10^9 halos collapse
+    Mhalo = 10 .^ range(6,9,length=10)
+    # structure formation era
+    z = [5 10]
+    direction = below
+    # Constraint passed if any halo passed
+    allow_any=true
+    return Properties(;Mhalo,z,direction,allow_any)
 end
 
 # Calculate the mass-loss constraint specified by properties
@@ -265,7 +289,7 @@ function calcMassLossConstraint(properties::Properties,adm::ADM)
     # (Looking at you getDBCProperties). Which takes forever to run 
     # get_Lambda on. So we'll reduce to 3 lines - max at every M, min at every
     # M, and median
-    if size(Lambda_cons)[2]>3
+    if size(Lambda_cons,2)>3
         max_inds = argmax(Lambda_cons,dims=2)
         med = median(Lambda_cons,dims=2)
         med_inds = argmin(abs.(Lambda_cons.-med),dims=2)
@@ -295,31 +319,71 @@ function checkMassLossConstraint(Lambda,properties,adm::ADM;n_test=nothing)
     if ndims(check)==2
         acheck = []
         for i = 1:size(check)[2]
-            push!(acheck,any(check[:,i]))
+            push!(acheck,all(check[:,i]))
         end
         return acheck
     elseif ndims(check)>2
         throw(ErrorException("I don't know how to handle $(ndims(check)) dimensions"))
     else
-        return any(check)
+        return all(check)
     end
 end
 
-function calcStrucFormConstraint(properties,adm::ADM)
-    throw(ErrorException("not implemented yet"))
+# Calculate the structure-formation constraint specified by properties
+# returns Lambda_cons, direction::cons_dir, n, T
+function calcStrucFormConstraint(properties::Properties,adm::ADM)
+    (;Mhalo, z, direction) = properties
+    n = nofz(adm.M,z)
+    T = Tv.(adm.M_mass .* n,Mhalo, adm.M_mass;dens_prof=2) # dens_prof=2 is equivalent to Buckley version
+    rho = adm.M_mass * n  # note this should include a factor of mu, but we'll assume it's 1
+    
+    Lambda_cons = struc_form_constraint.(E_dens.(n,T),tff.(rho),n)
+    if size(Lambda_cons,2)>3
+        max_inds = argmax(Lambda_cons,dims=2)
+        med = median(Lambda_cons,dims=2)
+        med_inds = argmin(abs.(Lambda_cons.-med),dims=2)
+        min_inds = argmin(Lambda_cons,dims=2)
+        inds = [min_inds med_inds max_inds]
+        return @views Lambda_cons[inds], direction, n[inds], T[inds]
+    end
+    
+    return Lambda_cons,direction,n,T
 end
 
-function checkStrucFormConstraint(Lambda,properties,adm::ADM;n_test=nothing)
-    throw(ErrorException("not implemented yet"))
-    if isnothing(n_test)
-        Lambda_test = Lambda.(n,T)
-    else
-        Lambda_test = Lambda.(n_test,T)
-    end
+function calcStrucFormConstraint(properties::Function,adm::ADM)
+    return calcStrucFormConstraint(properties(),adm)
+end
+
+# We'll duplicate for now, but we can probably combine with checkMassLossConstraint
+function checkStrucFormConstraint(Lambda,properties::Properties,adm::ADM;n_test=nothing)
     Lambda_cons, direction, n, T = calcStrucFormConstraint(properties,adm)
+    if isnothing(n_test)
+        Lambda_test = Lambda.(n,T,adm=adm)
+    else
+        Lambda_test = Lambda.(n_test,T,adm=adm)
+    end
     check = direction==above ? Lambda_test.<=Lambda_cons : Lambda_test.>=Lambda_cons
+    if properties.allow_any
+        any_all = any
+    else
+        any_all = all
+    end
+    if ndims(check)==2
+        acheck = []
+        for i = 1:size(check)[2]
+            push!(acheck,any_all(check[:,i]))
+        end
+        return acheck
+    elseif ndims(check)>2
+        throw(ErrorException("I don't know how to handle $(ndims(check)) dimensions"))
+    else
+        return any_all(check)
+    end
 end
 
+function checkStrucFormConstraint(Lambda,properties::Function,adm::ADM;n_test=nothing)
+    return checkStrucFormConstraint(Lambda,properties(),adm;n_test)
+end
 
 # Common function for constraint processing
 # Input: Lambda(n,T,dark_params), constraint_type (0=mass-loss,1=structure-formation)
@@ -332,28 +396,37 @@ function checkConstraint(Lambda,ct::constraint_type,properties,adm::ADM;n_test=n
     # way overkill. Short-circuiting is ok, but switch is way better
     validate_ADM(adm)
     ct==massloss && return checkMassLossConstraint(Lambda,properties,adm,;n_test)
-    ct==strucform && return checkStructFormConstraint(Lambda,properties,adm;n_test)
+    ct==strucform && return checkStrucFormConstraint(Lambda,properties,adm;n_test)
     throw(ArgumentError("$ct is not a valid constraint-type"))
 end
 
 function checkAllConstraints(Lambda,adm::ADM;n_test=nothing)
-    mlc = [getLSSProperties,getDwGalProperties,getDBCProperties]
-    names = ["LSS","DwG","DBC"]
+    mlc = [getLSSProperties,getDwGalProperties,getDBCProperties,getMWProperties,getDCOProperties]
+    names = ["LSS","DwG","DBC","MWG","DCO"]
+    types = [massloss,massloss,massloss,strucform,strucform]
     checks = Dict()
     for i in eachindex(mlc)
-        checks[names[i]] = checkConstraint(Lambda,massloss,mlc[i],adm;n_test)
+        checks[names[i]] = checkConstraint(Lambda,types[i],mlc[i],adm;n_test)
     end
     return checks
 end
 
 
-function plotConstraint(properties,adm::ADM;fig=nothing,name=nothing)
-    Lambda_cons, direction, n, T = calcMassLossConstraint(properties,adm)
+function plotConstraint(properties,adm::ADM;fig=nothing,name=nothing,ctype::constraint_type=massloss)
+    calcConstraint = ctype==massloss ? calcMassLossConstraint : calcStrucFormConstraint
+    Lambda_cons, direction, n, T = calcConstraint(properties,adm)
 
     if isnothing(name)
         name="Constraint"
     end
-    h = loglog(T, Lambda_cons,label=name)
+    if size(Lambda_cons,1)==1
+        marker="*"
+        linestyle="none"
+    else
+        marker="none"
+        linestyle=nothing
+    end
+    h = loglog(T, Lambda_cons;marker,linestyle,label=name)
     if size(h)[1]>1
         for h1 in h
             h1.update_from(h[1])
